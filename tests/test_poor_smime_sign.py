c86616ee58def9142d1932da60d4c3eab566cbcb
Julius Seporaitis <julius@seporaitis.net>
2015-11-27 19:16:26 +0000

    Improvements:
    - added (I think) pretty good test coverage.
    - `pyflakes`-ed and `pep8`-ed the package source (hence weird formatting).
    - added fixtures (`recipient.cert` is actually WWDR certificate); signer key/cert are files generated just for the tests. Will expire in 365 days.

--- tests/test_poor_smime_sign.py
+++ tests/test_poor_smime_sign.py
@@ -1,28 +1,254 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-test_poor-smime-sign
-----------------------------------
-
-Tests for `poor_smime_sign` module.
-"""
-
+from os import path
 import unittest
+import tempfile
 
-from poor_smime_sign import smime_sign
+from poor_smime_sign import smime_sign, smime_verify
 
 
-class SMIME_SIGNTest(unittest.TestCase):
+def _read_file(abs_path):
+        with file(abs_path, 'r') as f:
+            return f.read()
 
-    def setUp(self):
-        pass
 
-    def tearDown(self):
-        pass
+class PoorSmimeSignTest(unittest.TestCase):
 
-    def test_000_something(self):
-        pass
+    @classmethod
+    def setUpClass(cls):
+        cls.signer_cert_path = path.join(path.dirname(__file__), "files/signer.cert")
+        cls.signer_key_path = path.join(path.dirname(__file__), "files/signer.pem")
+        cls.recipient_cert_path = path.join(path.dirname(__file__), "files/recipient.cert")
+        cls.manifest_json_path = path.join(path.dirname(__file__), "files/manifest.json")
+
+        cls.signature_smime_path = path.join(path.dirname(__file__), "files/signature.smime")
+        cls.signature_pem_path = path.join(path.dirname(__file__), "files/signature.pem")
+        cls.signature_der_path = path.join(path.dirname(__file__), "files/signature.der")
+
+        cls.signature_smime = _read_file(cls.signature_smime_path)
+        cls.signature_pem = _read_file(cls.signature_pem_path)
+        cls.signature_der = _read_file(cls.signature_der_path)
+
+        cls.maniphest_json = _read_file(cls.manifest_json_path)
+
+    def test_smime_fixture(self):
+        self.assertTrue(
+            smime_verify(
+                signer_cert_path=self.signer_cert_path,
+                content_path=self.manifest_json_path,
+                signature_path=self.signature_smime_path,
+                signature_format='SMIME',
+            )
+        )
+
+    def test_pem_fixture(self):
+        self.assertTrue(
+            smime_verify(
+                signer_cert_path=self.signer_cert_path,
+                content_path=self.manifest_json_path,
+                signature_path=self.signature_pem_path,
+                signature_format='PEM',
+            )
+        )
+
+    def test_der_fixture(self):
+        self.assertTrue(
+            smime_verify(
+                signer_cert_path=self.signer_cert_path,
+                content_path=self.manifest_json_path,
+                signature_path=self.signature_der_path,
+                signature_format='DER',
+            )
+        )
+
+    def test_smime_format(self):
+        actual_smime = smime_sign(
+            signer_cert_path=self.signer_cert_path,
+            signer_key_path=self.signer_key_path,
+            recipient_cert_path=self.recipient_cert_path,
+            content=self.maniphest_json,
+            output_format='SMIME',
+        )
+
+        with tempfile.NamedTemporaryFile(delete=True) as temp:
+            temp.write(actual_smime)
+            temp.flush()
+
+            self.assertTrue(
+                smime_verify(
+                    signer_cert_path=self.signer_cert_path,
+                    content_path=self.manifest_json_path,
+                    signature_path=path.abspath(temp.name),
+                    signature_format='SMIME',
+                )
+            )
+
+    def test_pem_format(self):
+        actual_smime = smime_sign(
+            signer_cert_path=self.signer_cert_path,
+            signer_key_path=self.signer_key_path,
+            recipient_cert_path=self.recipient_cert_path,
+            content=self.maniphest_json,
+            output_format='PEM',
+        )
+
+        with tempfile.NamedTemporaryFile(delete=True) as temp:
+            temp.write(actual_smime)
+            temp.flush()
+
+            self.assertTrue(
+                smime_verify(
+                    signer_cert_path=self.signer_cert_path,
+                    content_path=self.manifest_json_path,
+                    signature_path=path.abspath(temp.name),
+                    signature_format='PEM',
+                )
+            )
+
+    def test_der_format(self):
+        actual_smime = smime_sign(
+            signer_cert_path=self.signer_cert_path,
+            signer_key_path=self.signer_key_path,
+            recipient_cert_path=self.recipient_cert_path,
+            content=self.maniphest_json,
+            output_format='DER',
+        )
+
+        with tempfile.NamedTemporaryFile(delete=True) as temp:
+            temp.write(actual_smime)
+            temp.flush()
+
+            self.assertTrue(
+                smime_verify(
+                    signer_cert_path=self.signer_cert_path,
+                    content_path=self.manifest_json_path,
+                    signature_path=path.abspath(temp.name),
+                    signature_format='DER',
+                )
+            )
+
+    def test_inconsistent_check(self):
+        actual_smime = smime_sign(
+            signer_cert_path=self.signer_cert_path,
+            signer_key_path=self.signer_key_path,
+            recipient_cert_path=self.recipient_cert_path,
+            content=self.maniphest_json,
+            output_format='SMIME',
+        )
+
+        with tempfile.NamedTemporaryFile(delete=True) as temp:
+            temp.write(actual_smime)
+            temp.flush()
+
+            self.assertFalse(
+                smime_verify(
+                    signer_cert_path=self.signer_cert_path,
+                    content_path=self.manifest_json_path,
+                    signature_path=path.abspath(temp.name),
+                    signature_format='PEM',
+                )
+            )
+
+    def test_relative_signer_cert_error(self):
+        relative_signer_cert = "files/certificate.pem"
+
+        with self.assertRaises(ValueError) as ctx:
+            smime_sign(
+                signer_cert_path=relative_signer_cert,
+                signer_key_path=self.signer_key_path,
+                recipient_cert_path=self.recipient_cert_path,
+                content=self.maniphest_json,
+                output_format='SMIME',
+            )
+
+        self.assertEqual(
+            ctx.exception.message,
+            "{file_list} must be absolute paths to existing files.".format(
+                file_list=", ".join([
+                    relative_signer_cert,
+                    self.signer_key_path,
+                    self.recipient_cert_path,
+                ]),
+            )
+        )
+
+    def test_relative_signer_key_error(self):
+        relative_signer_key = "files/private.pem"
+
+        with self.assertRaises(ValueError) as ctx:
+            smime_sign(
+                signer_cert_path=self.signer_cert_path,
+                signer_key_path=relative_signer_key,
+                recipient_cert_path=self.recipient_cert_path,
+                content=self.maniphest_json,
+                output_format='SMIME',
+            )
+
+        self.assertEqual(
+            ctx.exception.message,
+            "{file_list} must be absolute paths to existing files.".format(
+                file_list=", ".join([
+                    self.signer_cert_path,
+                    relative_signer_key,
+                    self.recipient_cert_path,
+                ]),
+            )
+        )
+
+    def test_relative_recipient_cert_error(self):
+        relative_recipient_cert = "files/wwdr.pem"
+
+        with self.assertRaises(ValueError) as ctx:
+            smime_sign(
+                signer_cert_path=self.signer_cert_path,
+                signer_key_path=self.signer_key_path,
+                recipient_cert_path=relative_recipient_cert,
+                content=self.maniphest_json,
+                output_format='SMIME',
+            )
+
+        self.assertEqual(
+            ctx.exception.message,
+            "{file_list} must be absolute paths to existing files.".format(
+                file_list=", ".join([
+                    self.signer_cert_path,
+                    self.signer_key_path,
+                    relative_recipient_cert,
+                ]),
+            )
+        )
+
+    def test_incorrect_format_error(self):
+        with self.assertRaises(ValueError) as ctx:
+            smime_sign(
+                signer_cert_path=self.signer_cert_path,
+                signer_key_path=self.signer_key_path,
+                recipient_cert_path=self.recipient_cert_path,
+                content=self.maniphest_json,
+                output_format='INCORRECT',
+            )
+
+        self.assertEqual(
+            ctx.exception.message,
+            ("{output_format}' not found in the set of supported "
+             "formats: {supported_formats}").format(
+                 output_format='INCORRECT',
+                 supported_formats=", ".join({'SMIME', 'PEM', 'DER'}),
+            )
+        )
+
+    def test_runtime_error(self):
+        with self.assertRaises(RuntimeError) as ctx:
+            smime_sign(
+                signer_cert_path=self.signer_key_path,
+                signer_key_path=self.recipient_cert_path,
+                recipient_cert_path=self.signer_key_path,
+                content=self.maniphest_json,
+                output_format='PEM',
+            )
+
+        self.assertRegexpMatches(ctx.exception.message, r'OpenSSL failed with #2: .*')
 
 
 if __name__ == '__main__':
